03dc6f41e633f23a147fb0ce1d50a5a0482eb5b0
Viktoriia <freshitpte@gmail.com>
2024-09-07 17:32:30 +0300

    Add controllers

---
+++ controllers/contactsController.js
@@ -0,0 +1,13 @@
+module.exports.createContact = (req, res) => {
+
+};
+
+module.exports.getContacts = (req, res) => {
+  res.status(200).send('OK!!!!!!');
+};
+
+module.exports.getContactById = (req, res) => {};
+
+module.exports.updateContactById = (req, res) => {};
+
+module.exports.deleteContactById = (req, res) => {};
---
+++ controllers/index.js
@@ -0,0 +1,3 @@
+module.exports.contactsController = require('./contactsController');
+module.exports.authController = require('./authController');
+module.exports.messageController = require('./messageController');
