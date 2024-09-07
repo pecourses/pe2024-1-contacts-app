5d79252c3790ddc30fb577277104cca9c0b4490f
Viktoriia <freshitpte@gmail.com>
2024-09-07 19:39:40 +0300

    Add mw, next

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { contactsController } = require('./controllers');
-
+const yup = require('yup');
 const app = express();
 
 app.use(express.json());
@@ -30,8 +30,41 @@ app.use(express.json());
 // GET http://localhost:5000/contacts?page=1&results=3
 app.get('/contacts', contactsController.getContacts);
 
+const CREATE_CONTACT_VALIDATION_SCHEMA = yup.object({
+  name: yup
+    .string()
+    .trim()
+    .min(2)
+    .max(16)
+    .matches(/^[A-Z][a-z]{1,15}$/)
+    .required(),
+  telNumber: yup
+    .string()
+    .trim()
+    .length(13)
+    .matches(/^\+380\d{9}$/)
+    .required(),
+  birthday: yup.date().max(new Date()),
+});
+const validateContactOnCreate = async (req, res, next) => {
+  const { body } = req;
+  try {
+    const validatedContact = await CREATE_CONTACT_VALIDATION_SCHEMA.validate(
+      body
+    );
+    req.body = validatedContact;
+    next();
+  } catch (err) {
+    res.status(422).send(err.errors);
+  }
+};
+
 // POST http://localhost:5000/contacts
-app.post('/contacts', contactsController.createContact);
+app.post(
+  '/contacts',
+  validateContactOnCreate,
+  contactsController.createContact
+);
 
 // GET http://localhost:5000/contacts/555654
 app.get('/contacts/:id', contactsController.getContactById);
